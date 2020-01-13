404c8636b9106edcd16d87a6366292394aeddb42
arnie b <arnold.bwaila@gmail.com>
2020-01-13 21:59:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,14 +11,20 @@ class Triangle
   
   def valid_triangle
     valid = false
+<<<<<<< HEAD
     if (((@sidea + @sideb) > @sidec) &&  ((@sidea + @sidec) > @sideb) &&  ((@sidec + @sideb) > @sidea))
      if ((@sidea > 0) && (@sideb > 0) && (@sidec > 0))
        valid = true
      end
+=======
+    if (((@sidea + @sideb) < @sidec) ||  ((@sidea + @sidec) < @sideb) ||  ((@sidec + @sideb) < @sidea))
+     if @sidea > 0 && sideb > 0 && sidec {valid = false}
+>>>>>>> f1d0689c20aab1661e5c5e70ebc31b3f1afbaa94
     end
     valid
   end
     
+<<<<<<< HEAD
   def kind
     if !valid_triangle
         raise TriangleError
@@ -35,6 +41,12 @@ class Triangle
  
   class TriangleError < StandardError
   
+=======
+
+  
+  class TriangleError < StandardError
+   
+>>>>>>> f1d0689c20aab1661e5c5e70ebc31b3f1afbaa94
   end
   
 end
